b2131002a620b231754b5ec099f52949fbac8319
arinamalygina <160202011+arinamalygina@users.noreply.github.com>
2024-02-25 16:27:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-	while (b != 0) {
-		int t = b;
-		b = a % b;
-		a = t;
-	}
-	return a;
+    // поместить сюда текст реализации алгоритма (на С++)
+    while (a != b) {
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
+    }
+    return a;
 }
